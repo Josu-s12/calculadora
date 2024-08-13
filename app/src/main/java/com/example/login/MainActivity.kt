9735bf0c8cf0fae877e48ca376c613494f6b3d37
Josue Sarmiento <josuesagu@unisabana.edu.co>
2024-08-13 09:05:09 -0500

    crear usuario

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,30 +1,28 @@
 package com.example.login
 
-import android.icu.text.DecimalFormat
+import java.text.DecimalFormat
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     val SUMA = "+"
     val RESTA = "-"
     val MULTIPLICACION = "*"
+    val DIVISION = "/"
     val PORCENTAJE = "%"
 
     var operacionActual = ""
 
-    var primerNumero:Double = Double.NaN
-    var segundoNumero:Double = Double.NaN
+    var primerNumero : Double = Double.NaN
+    var segundoNumero : Double = Double.NaN
 
     lateinit var tvTemp: TextView
     lateinit var tvResult: TextView
 
-    lateinit var formatoDecimal: DecimalFormat
+    lateinit var formatoDecimal : DecimalFormat
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,19 +35,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun cambiarOperador(b: View){
-        if(tvTemp.text.isNotEmpty() || primerNumero.toString()!="NaN"){
+        if (tvTemp.text.isNotEmpty() || primerNumero.toString()!="NaN"){
 
             calcular()
             val boton: Button = b as Button
-            if(boton.text.toString().trim()=="÷"){
+            if (boton.text.toString().trim() == "/"){
                 operacionActual = "/"
-            }else if(boton.text.toString().trim()=="x"){
+            }else if (boton.text.toString().trim() == "X"){
                 operacionActual = "*"
             }else{
                 operacionActual = boton.text.toString().trim()
             }
-            if(tvTemp.text.toString().isEmpty()){
-                tvTemp.text=tvResult.text
+            if (tvTemp.text.toString().isEmpty()){
+                tvTemp.text = tvResult.text
             }
 
             tvResult.text = formatoDecimal.format(primerNumero) + operacionActual
@@ -59,15 +57,14 @@ class MainActivity : AppCompatActivity() {
 
     fun calcular(){
         try {
-            if (primerNumero.toString() != "NaN") {
-                if (tvTemp.text.toString().isEmpty()) {
+            if (primerNumero.toString()!="NaN"){
+                if (tvTemp.text.toString().isEmpty()){
                     tvTemp.text = tvResult.text.toString()
                 }
-
                 segundoNumero = tvTemp.text.toString().toDouble()
-                tvTemp.text=""
+                tvTemp.text = ""
 
-                when(operacionActual) {
+                when(operacionActual){
                     "+" -> primerNumero = (primerNumero + segundoNumero)
                     "-" -> primerNumero = (primerNumero - segundoNumero)
                     "*" -> primerNumero = (primerNumero * segundoNumero)
@@ -82,8 +79,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun seleccionarNumero(b: View){
-        val boton: Button = b as Button
+    fun seleccionarNumero(b : View){
+        val boton : Button = b as Button
         tvTemp.text = tvTemp.text.toString() + boton.text.toString()
     }
 
@@ -94,25 +91,25 @@ class MainActivity : AppCompatActivity() {
         operacionActual = ""
     }
 
-    fun borrar(b: View){
-        val boton: Button = b as Button
-        if(boton.text.toString().trim()=="C"){
+    fun borrar(b : View){
+        val boton : Button = b as Button
+        if (boton.text.toString().trim() == "C"){
 
             if (tvTemp.text.toString().isNotEmpty()){
-                var datosActuales:CharSequence = tvTemp.text as CharSequence
-                tvTemp.text = datosActuales.subSequence(0,datosActuales.length-1)
+                var datosActuales: CharSequence = tvTemp.text as CharSequence
+                tvTemp.text = datosActuales.subSequence(0, datosActuales.length-1)
             }else{
                 primerNumero = Double.NaN
                 segundoNumero = Double.NaN
                 tvTemp.text = ""
                 tvResult.text = ""
             }
-        }else if (boton.text.toString().trim()=="AC"){
+        } else if (boton.text.toString().trim() == "CA"){
             primerNumero = Double.NaN
             segundoNumero = Double.NaN
             tvTemp.text = ""
             tvResult.text = ""
         }
-    }
 
     }
+}
--- app/src/main/java/com/example/login/signup.kt
+++ app/src/main/java/com/example/login/signup.kt
@@ -1,15 +1,76 @@
 package com.example.login
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import android.content.Intent
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
 
 class signup : AppCompatActivity() {
+
+    private lateinit var editName: EditText
+    private lateinit var editEmail: EditText
+    private lateinit var editPassword: EditText
+    private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
+
+        editName = findViewById(R.id.edt_nombre)
+        editEmail = findViewById(R.id.edt_email)
+        editPassword = findViewById(R.id.edt_password)
+        btnSignUp = findViewById(R.id.btn_SingUp)
+
+        btnSignUp.setOnClickListener{
+            val name = editName.text.toString()
+            val email = editEmail.text.toString()
+            val password = editPassword.text.toString()
+
+            singUp(name,email,password);
+        }
+    }
+
+    private  fun singUp( name: String,email: String, password: String){
+        //logica para crear usuarios
+        mAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this) { task ->
+                if(task.isSuccessful){
+
+                    addUserToDatabase(name,email,mAuth.currentUser?.uid!!)
+
+                    val intent = Intent(this@signup, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+
+                }else{
+                    Toast.makeText(
+                        this@signup,
+                        "ha ocurrido un error.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
+    private fun addUserToDatabase (name: String,email: String, uid: String){
+
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        mDbRef.child("user").child(uid).setValue(User(name, email, uid))
 
     }
 }
